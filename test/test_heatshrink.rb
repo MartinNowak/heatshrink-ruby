610c9ea97537cfbaf1e90eb8d5f3790d4e18b939
Martin Nowak <code@dawg.eu>
2015-06-24 16:55:12 +0200

    update heatshrink C sources to 0.4.0

--- test/test_heatshrink.rb
+++ test/test_heatshrink.rb
@@ -7,7 +7,7 @@ class HeatshrinkTest < Minitest::Test
     50.times do
       data = SecureRandom.random_bytes(rand(2000))
       [8, 9, 10, 11, 12].each do |ws|
-        [2, 3, 4, 5, 6, 7].each do |la|
+        [3, 4, 5, 6, 7].each do |la|
           data2 = Heatshrink.compress(data, ws, la)
           assert_equal data, Heatshrink.uncompress(data2, ws, la)
         end
